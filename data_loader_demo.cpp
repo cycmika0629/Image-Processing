2caf986a872614d01c2c9d7a7aaa336d7578249e
therealczr <125191151+therealczr15@users.noreply.github.com>
2025-05-09 18:49:44 +0800

    Update data_loader_demo.cpp

--- data_loader_demo.cpp
+++ data_loader_demo.cpp
@@ -11,7 +11,6 @@ int main(){
     data_loader.Dump_Gray(w1, h1, pixels1, string("pixels1.jpg"));
     data_loader.Display_Gray_X_Server(w1, h1, pixels1);
     data_loader.Display_Gray_ASCII(w1, h1, pixels1);
-    data_loader.Display_Gray_CMD("pixels1.jpg");
 
     // 2. Load rgb image into gray scale image
     int w2;
@@ -19,7 +18,6 @@ int main(){
     int **pixels2 = data_loader.Load_Gray("Image-Folder/lena.jpg", &w2, &h2);
     data_loader.Dump_Gray(w2, h2, pixels2, string("pixels2.jpg"));
     data_loader.Display_Gray_X_Server(w2, h2, pixels2);
-    data_loader.Display_Gray_CMD("pixels2.jpg");
 
     // 3. Load rgb image
     int w3;
@@ -28,7 +26,6 @@ int main(){
     data_loader.Dump_RGB(w3, h3, pixels3, string("pixels3.jpg"));
     data_loader.Display_RGB_X_Server(w3, h3, pixels3);
     data_loader.Display_RGB_ASCII(w3, h3, pixels3);
-    data_loader.Display_RGB_CMD("pixels3.jpg");
 
     // 4. List Directory
     vector<string> filenames;
