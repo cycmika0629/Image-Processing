9bb0f896f465b8248e9340f6e9e04aaff234285d
cycmika0629 <apple100079@gmail.com>
2025-06-08 12:14:19 +0800

    gray_image.cpp

--- src/gray_image.cpp
+++ src/gray_image.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
   GrayImage::GrayImage(): Image(0, 0), pixels(nullptr){}
   GrayImage::GrayImage(int w, int h, int **p): Image(w, h), pixels(p){}
-  GrayImage::~GrayImage(){
-    if (pixels){
-      for (int i = 0; i < width; i++){
-        delete[] pixels[i];
-      }
-      delete[] pixels;
+  GrayImage::~GrayImage() {
+    if (pixels) {
+        for (int i = 0; i < height; ++i) {
+            delete[] pixels[i];
+        }
+        delete[] pixels;
     }
-  }
+}
+
   bool GrayImage::LoadImage(string filename){
     Data_Loader loader;
     pixels = loader.Load_Gray(filename, &width, &height);
@@ -32,4 +33,3 @@ using namespace std;
     display.Display_Gray_ASCII(width, height, pixels);
     return;
   }
-  /*void GrayImage::Display_CMD();*/
